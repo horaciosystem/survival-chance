ae6ea5e490d3c0611bbc4e9d856f0024cfc37113
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2019-01-15 13:22:58 -0200

    Add reakit component library

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Provider as ThemeProvider } from "reakit"
+import theme from "reakit-theme-default"
 import SurvivorsList from "./pages/SurvivorsList"
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <>
-          <Route path="/survivals" exact component={SurvivorsList} />
-        </>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <>
+            <Route path="/survivals" exact component={SurvivorsList} />
+          </>
+        </Router>
+      </ThemeProvider>
     )
   }
 }
